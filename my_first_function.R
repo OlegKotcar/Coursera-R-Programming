1331d76deb258fc68fa8e82272e91ddbfb5440ef
Oleg Kotcar <jonin.j@gmail.com>
2014-11-07 15:19:11 +0900

    R programming course Week1

--- my_first_function.R
+++ my_first_function.R
@@ -2,3 +2,21 @@ add2 <- function(x,y){
   x+y
 }
 
+above10 <- function(x){
+  use <- x > 10
+  x[use]
+}
+
+above <- function(x, n){
+  use <- x > n
+  x[use]
+}
+
+columnmean <- function(x, rmNA = TRUE){
+  numcol <- ncol(x)
+  means <- numeric(numcol)
+  for(i in 1:numcol) {
+    means[i] <- mean(x[, i], na.rm = rmNA)
+  }  
+  means
+}
